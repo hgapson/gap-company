e6096715989172d09a417ddc676ce7c0785d336b
hgapson <hgapson@gmail.com>
2026-09-10 10:38:39 +1200

    changing logo name

--- src/components/Brand.jsx
+++ src/components/Brand.jsx
@@ -1,3 +1,3 @@
 export default function Brand() {
-  return <a className="brand" href="#top" aria-label="Gapson Tech Solutions home"><span className="brand-mark">G</span><span>Gapson <b>Tech</b></span></a>
+  return <a className="brand" href="#top" aria-label="Gapson Tech Solutions home"><span className="brand-mark">G</span><span>Gap <b>Tech</b></span></a>
 }
